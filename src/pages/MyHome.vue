a8e2d2d224bb23bfdd23546c0b0d48b47935b5c4
bambbang2 <4900ekrud@naver.com>
2024-05-13 21:12:48 +0900

    init

---
+++ src/pages/MyHome.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <section class="py-5 text-center container">
+      <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <h1 class="fw-light">Album example</h1>
+          <p class="lead text-body-secondary">
+            Something short and leading about the collection below—its contents,
+            the creator, etc. Make it short and sweet, but not too short so
+            folks don’t simply skip over it entirely.
+          </p>
+          <p>
+            <a href="#" class="btn btn-primary my-2">Main call to action</a>
+            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <div class="album py-5 bg-body-tertiary">
+      <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <div class="col" v-for="(item, index) in items" :key="index">
+            <PdCard :item="item" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// ===================================================================
+// import
+// ===================================================================
+
+import PdCard from "@/components/PdCard.vue";
+import axios from "axios";
+import { ref } from "vue";
+
+// ===================================================================
+// function
+// ===================================================================
+
+let items = ref({});
+
+async function fetchData() {
+  try {
+    const res = await axios.get("/api/product/list");
+    items.value = res.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+// Initial data fetching
+fetchData();
+
+</script>
+
+<style scoped></style>
+
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   outputDir: '../src/main/resources/static', // Build Directory
+// 	devServer: {
+// 		proxy: 'http://localhost:8080/login' // Spring Boot Server
+// 	}
+// })
+
+module.exports = {
+  // npm run build 타겟 디렉토리
+  outputDir: '../backend/src/main/resources/static',
+
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+      },
+    },
+  },
+};
